989de898efe68723773dc44dae7f92c7b4ea5f55
Andres Aguayo <andyaguayo49@gmail.com>
2026-05-11 14:07:31 -0300

    Flag actualizada. Comprobacion not servicios eliminada

--- controllers/serviciosController.js
+++ controllers/serviciosController.js
@@ -6,13 +6,10 @@ const getServicios = async (req, res) => {
     // Buscar los datos en el json de "data"
     console.log('- Buscando datos -')
     const data = await fs.readFile('./data/servicios.json', 'utf-8')
-    const servicios = JSON.parse(data)
 
-    // Comprobar si el json esta vacio
-    console.log('- Comprobando datos -')
-    if (!servicios) {
-      return res.status(404).json({ error: 'No se encontraron los servicios' })
-    }
+    // Parsear el json recibido
+    console.log('- Parseando informacion -')
+    const servicios = JSON.parse(data)
 
     // Devolver el json con los datos
     console.log('- Datos enviados con exito -')
