29ff577a284f874d3b1ea6a8bc1d9e46506d1758
Andres Aguayo <andyaguayo49@gmail.com>
2026-05-11 14:01:01 -0300

    Reorganizacion de flags

--- controllers/serviciosController.js
+++ controllers/serviciosController.js
@@ -4,17 +4,18 @@ const fs = require('fs').promises
 const getServicios = async (req, res) => {
   try {
     // Buscar los datos en el json de "data"
+    console.log('- Buscando datos -')
     const data = await fs.readFile('./data/servicios.json', 'utf-8')
-    console.log('Buscando datos')
     const servicios = JSON.parse(data)
 
     // Comprobar si el json esta vacio
-    console.log('Comprobando datos')
+    console.log('- Comprobando datos -')
     if (!servicios) {
       return res.status(404).json({ error: 'No se encontraron los servicios' })
     }
 
     // Devolver el json con los datos
+    console.log('- Datos enviados con exito -')
     return res.status(200).json(servicios)
   } catch (error) {
     console.log(error)
