a7aa92cc05ec0715aa6d41250bb06a27dbfc37bb
Andres Aguayo <andyaguayo49@gmail.com>
2026-05-13 19:26:21 -0300

    Arreglos de coherencia

--- controllers/equipoController.js
+++ controllers/equipoController.js
@@ -16,27 +16,4 @@ const getEquipo = async (req, res) => {
   }
 }
 
-const getEquipoById = async (req, res) => {
-  const { id } = req.params
-  try {
-    const data = await fs.readFile('./data/equipo.json', 'utf-8')
-    const equipo = JSON.parse(data)
-
-    const equipoId = equipo.find((e) => e.id === parseInt(id))
-
-    if (!equipoId) {
-      return res
-        .status(404)
-        .json({ msg: `No se encontró el equipo con id ${id}.` })
-    }
-
-    return res.status(200).json(equipoId)
-  } catch (error) {
-    console.log(error)
-    return res
-      .status(500)
-      .json({ error: `No se pudo obtener el detalle del id n° ${id}` })
-  }
-}
-
-module.exports = { getEquipo, getEquipoById }
+module.exports = { getEquipo }
